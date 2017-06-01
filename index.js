ed12aba101239e86521f17eeb73153807cf1ea03
srsajjad <sajjadsr001@gmail.com>
2017-06-01 09:37:32 +0600

    fixed-height-problem

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-  var ul = document.querySelector('#ul');
+  var ul = document.querySelector('#ul')
 
-  reddit.hot('wallpapers').limit(25).fetch(function(res) {
+  reddit.hot('wallpapers').limit(25).fetch(function (res) {
     
-
     var linkArr = res.data.children
-                    .filter((n)=>n.data.url.endsWith('.jpg'))
-                    .slice(0,9)
-                    .forEach((n,i)=>{
-                        var url = n.data.url ;
-                        var a = document.createElement('a');
-                        var li = document.createElement('div');
-                        var img = document.createElement('img');
+      .filter((n) => n.data.url.endsWith('.jpg'))
+      .slice(0, 9)
+      .forEach((n, i) => {
+        var url = n.data.url
+        var a = document.createElement('a')
+        var li = document.createElement('div')
+        var img = document.createElement('img')
 
-                        a.href = url ;
-                        img.src = url ;
+        a.href = url
+        img.src = url
 
-                        a.setAttribute('data-lightbox','test');
-                        li.setAttribute('class','li');
+        a.setAttribute('data-lightbox', 'test')
+        li.setAttribute('class', 'li')
 
-                        a.appendChild(img);
-                        li.appendChild(a);
-                        ul.appendChild(li);
-                    })
+        a.appendChild(img)
+        li.appendChild(a)
+        ul.appendChild(li)
+      })
 
-  });
+  })
